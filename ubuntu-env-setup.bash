2679bda172e4ad111eab18c6d27e76ce9deef23b
Ryan McClue <re.mcclue@protonmail.com>
2021-01-22 10:16:31 +1100

    Added SSH support for git

--- ubuntu-env-setup.bash
+++ ubuntu-env-setup.bash
@@ -3,55 +3,50 @@
 
 red='\033[31m'
 green='\033[32m'
-yellow='\033[33m'
 reset='\033[0m'
 
-if [[ $EUID -ne 0 ]]; then
-  printf "${red}MUST BE ROOT!\n${reset}"
+if [[ $EUID -eq 0 ]]; then
+  printf "${red}CANNOT BE ROOT TO IMPORT GPG/SSH KEYS FOR USER!\n${reset}"
   exit 1
 fi
 
 if [[ ! -f private-key.asc ]]; then
-  printf "${red}Require GPG private-key.asc to be in $(cwd)\n${reset}"
+  printf "${red}REQUIRE GPG PRIVATE-KEY.ASC TO BE IN $(cwd)\n${reset}"
   exit 2
 fi
 
 if [[ ! -f .ssh/id_rsa || ! -f .ssh/id_rsa.pub ]]; then
-  printf "${red}Require SSH .ssh folder to be populated $(cwd)\n${reset}"
+  printf "${red}REQUIRE .SSH FOLDER TO BE POPULATED IN $(cwd)\n${reset}"
   exit 3
 fi
 
-printf "${green}INSTALLING APPLICATIONS\n${reset}"
-sudo apt update
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-
-sudo apt install gvim wmctrl gcc git dconf-cli 
-
-exec bash
-
 printf "${green}CONFIGURING GNOME TERMINAL AND BASH\n${reset}"
 git clone https://github.com/aruhier/gnome-terminal-colors-solarized.git
-cd gnome-terminal-colors-solarized
+pushd gnome-terminal-colors-solarized
 ./install.sh --install-dircolors
-./set-light.sh
+popd
+rm -rf gnome-terminal-colors-solarized
+
+cp codeclap.desktop ~/.local/share/applications/
+mkdir -p ~/.local/share/icons
+cp codeclap-icon.png ~/.local/share/icons/
+
 
 mkdir ~/.bash
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.bash/git-prompt.sh
 
 cp .bashrc ~/.bashrc
 
-mkdir -p ~/prog/personal
+mkdir -p ~/prog/{personal,apps}
 
-exec bash
 
 printf "${green}CONFIGURING GPG AND SSH\n${reset}"
 gpg --import private-key.asc
-cp -r .ssh ~/.ssh 
+mkdir ~/.ssh
+cp -r .ssh/. ~/.ssh 
+chmod 600 ~/.ssh/id_rsa 
 ssh-add
 
-exec bash
 
 printf "${green}CONFIGURING VIM\n${reset}"
 wget https://raw.githubusercontent.com/ryan-mcclue/cas/main/.vimrc -O ~/.gvimrc
@@ -59,6 +54,7 @@ wget https://raw.githubusercontent.com/ryan-mcclue/cas/main/.vimrc -O ~/.vimrc
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim \
   -O ~/.vim/colors/solarized.vim
+
 
 printf "${green}CONFIGURING GIT\n${reset}"
 git config --global user.name "Ryan McClue"
@@ -77,8 +73,3 @@ git config --global difftool.vimdiff.cmd 'vim -f -d -c "wincmd h" -c '\''cd "$GI
 # NOTE(Ryan): This allows the use of :cquit to exit the entire diff process. $
 git config --global difftool.trustExitCode true
 git config --global difftool.prompt false
-
-
-printf "${yellow}-------------------------------\n${reset}"
-printf "${yellow}-- MANUALLY INSTALL CODECLAP --\n${reset}"
-printf "${yellow}-------------------------------\n${reset}"
